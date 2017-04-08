6d238c5e4f41e9dca838b3d2095ef0002e780086
mariela <marieladaniele@gmail.com>
2017-04-08 15:12:58 -0300

    Mi app Django Girls, primer commit

---
+++ python_intro.py
@@ -0,0 +1,55 @@
+#print('Hello, Django grils')
+#
+#
+#if 5 > 2:
+#	print('5 is indeed greater than 2')
+#else:
+#	print('5 is not greater than 2')
+#
+#
+#
+#name = 'mariel'
+#if name == 'alejandra':
+#	print('hey alejandra')
+#elif name == 'mariela':
+#	print('hey mariela')
+#else:
+#	print('hey anonymous!')
+#
+#
+#def hi():
+#	print('hi there!')
+#	print('How are you?')
+#hi()
+#
+#
+#def hi(name):
+#	if name == 'mariela':
+#		print('hi mariela!')
+#	elif name == 'alejandra':
+#		print('hi alejandra!')
+#	else:
+#		print('hi anonymous')
+#hi("pedro")
+#
+#
+#
+#def hi(name):
+#	print('hi ' + name + '!')
+#
+#hi("marielita")
+#
+#
+#
+#def hi(name):
+#	print('hi ' + name + '!')
+#
+#girls = ['mariela','alejandra','mariana','fernanda']
+#for name in girls:
+#	hi(name)
+#	print('next girls')	
+#
+#
+#
+for i in range(1,6):
+	print(i)
